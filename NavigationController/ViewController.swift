8aee358772a65354dbf9d6077e57a17ca237022f
Soha <Soha.Mourad@triosstudent.com>
2019-05-30 18:41:12 -0400

    Initial Commit

---
+++ NavigationController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationController
+//
+//  Created by Soha Mourad on 2019-05-30.
+//  Copyright © 2019 Soha Mourad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
