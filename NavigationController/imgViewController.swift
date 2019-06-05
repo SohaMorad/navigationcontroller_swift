a588b0818cec360f7dcc73380ea3c7217cc5a3c1
Soha <Soha.Mourad@triosstudent.com>
2019-06-05 19:40:38 -0400

    first commit

---
+++ NavigationController/imgViewController.swift
@@ -0,0 +1,30 @@
+//
+//  imgViewController.swift
+//  NavigationController
+//
+//  Created by Soha Mourad on 2019-05-30.
+//  Copyright © 2019 Soha Mourad. All rights reserved.
+//
+
+import UIKit
+
+class imgViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
